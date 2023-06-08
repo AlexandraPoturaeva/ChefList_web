83ef99f32d92b50f0ab45a269ca5478ceb8f0626
AlexandraPoturaeva <112418003+AlexandraPoturaeva@users.noreply.github.com>
2023-06-08 12:43:42 +0300

    flask-sqlalchemy and User model setting (#5)
    * installation of flask-SQLAlchemy, creation of User Model and database
    
    * some pseudo testing for User Model added
    
    * empty lines added
    
    * 1. Assertion added to test_add_user.py
    2. Password_hash was removed from "__repr__" method of User Model

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,14 @@
+import os
+
 from flask import Flask, render_template
+from webapp.model import db
 
 
 def create_app():
+    basedir = os.path.abspath(os.path.dirname(__file__))
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+    db.init_app(app)
 
     @app.route("/")
     def index():
