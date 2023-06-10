bb29a71f1ca3641334d650c05758e96af1c83153
AlexandraPoturaeva <112418003+AlexandraPoturaeva@users.noreply.github.com>
2023-06-10 11:44:07 +0300

    Registration (#6)
    * 1. FlaskForm for registration created
    2. html for registration was changed
    3. Routes for registration and processing of registration created
    
    * New dependencies added to requirements.txt
    
    * 1. Page title for registration page moved directly to registration.html
    2. Field created_at in User Model changed: now it gets datetime.utcnow instead of time on DB side
    
    * create config.py.sample
    
    * add empty line to config.py.sample

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,18 +1,40 @@
-import os
-
-from flask import Flask, render_template
-from webapp.model import db
+from flask import Flask, flash, redirect, render_template, url_for
+from webapp.forms import RegistrationForm
+from webapp.model import db, User
 
 
 def create_app():
-    basedir = os.path.abspath(os.path.dirname(__file__))
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+    app.config.from_pyfile('config.py')
     db.init_app(app)
 
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    @app.route("/registration")
+    def registration():
+        form = RegistrationForm()
+        return render_template('registration.html', form=form)
+
+    @app.route("/process-reg", methods=['POST'])
+    def process_reg():
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            new_user = User(name=form.name.data, email=form.email.data)
+            new_user.set_password(form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Вы успешно зарегистрировались')
+            return redirect(url_for('index'))
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    flash('Ошибка в поле "{}": {}'.format(
+                        getattr(form, field).label.text,
+                        error
+                    ))
+        return redirect(url_for('registration'))
 
     return app
 
--- webapp/model.py
+++ webapp/model.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -8,7 +10,13 @@ class User(db.Model):
     email = db.Column(db.Text, nullable=False, unique=True)
     password_hash = db.Column(db.Text, nullable=False)
     name = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'User {self.id} ' \
