78b97dd154fbac8d509d6876cf953a8260915a85
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-06-13 13:21:53 +0300

    create login function

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, flash, redirect, render_template, url_for
-from webapp.forms import RegistrationForm
+from flask_login import LoginManager, login_user
+from webapp.forms import LoginForm, RegistrationForm
 from webapp.model import db, User
 
 
@@ -7,6 +8,13 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
 
     @app.route("/")
     def index():
@@ -35,6 +43,28 @@ def create_app():
                         error
                     ))
         return redirect(url_for('registration'))
+
+    @app.route('/login')
+    def login():
+        form = LoginForm()
+        return render_template('login.html', form=form)
+
+    @app.route('/profile', methods=['POST'])
+    def profile():
+        form = LoginForm()
+
+        if form.validate_on_submit():
+            user = User.query.filter(User.email == form.email.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы успешно вошли на сайт')
+                return f'<h1> ' \
+                       f'Логин: {user.email} <br> ' \
+                       f'Дата регистрации: {user.created_at.strftime("%d.%m.%Y")}' \
+                       f'</h1>'
+
+        flash('Неверное имя и/или пароль')
+        return redirect(url_for('login', next='profile'))
 
     return app
 
