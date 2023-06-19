3870611ffd09c63efed4831c509f0489af050daf
AlexandraPoturaeva <112418003+AlexandraPoturaeva@users.noreply.github.com>
2023-06-19 11:28:52 +0300

    develop creation of shopping list (#8)

--- webapp/model.py
+++ webapp/model.py
@@ -7,11 +7,13 @@ db = SQLAlchemy()
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.Text, nullable=False, unique=True)
     password_hash = db.Column(db.Text, nullable=False)
     name = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    shopping_lists = db.relationship('ShoppingList', backref='user', lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -24,3 +26,24 @@ class User(db.Model, UserMixin):
                f'\nemail: {self.email} ' \
                f'\nname: {self.name}' \
                f'\ncreated_at {self.created_at}'
+
+
+class ShoppingList(db.Model):
+    __tablename__ = 'shopping_list'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    shopping_items = db.relationship('ShoppingItem', backref='shopping_list', lazy=True)
+    public_id = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+
+class ShoppingItem(db.Model):
+    __tablename__ = 'shopping_item'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    shopping_list_id = db.Column(db.Integer, db.ForeignKey('shopping_list.id'), nullable=False)
+    quantity = db.Column(db.Float)
+    selected = db.Column(db.Boolean, default=False)
+
+
