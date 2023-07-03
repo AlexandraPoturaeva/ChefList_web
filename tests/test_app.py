7d1490b08380a602faa536d8346040ce155df6d7
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-07-03 16:13:44 +0300

    rename test_status.py to test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,8 +7,7 @@ def client():
     app = create_app()
     app.config["TESTING"] = True
 
-    with app.test_client() as client:
-        yield client
+    return app.test_client()
 
 
 def test_status_index(client):
